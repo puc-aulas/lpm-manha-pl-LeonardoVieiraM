5611a4ff39a79bf72cc4111d801c04a4c2d9e8cf
coutinholaura <110981345+coutinholaura@users.noreply.github.com>
2023-09-05 15:45:56 -0300

    Add files via upload

---
+++ AlugueldeEquipamentos/src/AlugueldeEquipamentos/Aluguel.java
@@ -0,0 +1,27 @@
+package AlugueldeEquipamentos;
+import java.util.Date;
+
+public class Aluguel extends Equipamentos {
+	 private Date dataInicio;
+	 private Date dataFim;
+	 private Cliente cliente;
+	 
+	public Date getDataInicio() {
+		return dataInicio;
+	}
+	public void setDataInicio(Date dataInicio) {
+		this.dataInicio = dataInicio;
+	}
+	public Date getDataFim() {
+		return dataFim;
+	}
+	public void setDataFim(Date dataFim) {
+		this.dataFim = dataFim;
+	}
+	public Cliente getCliente() {
+		return cliente;
+	}
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+}
---
+++ AlugueldeEquipamentos/src/AlugueldeEquipamentos/AluguelTest.java
@@ -0,0 +1,39 @@
+package AlugueldeEquipamentos;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class AluguelTest {
+
+	@Test
+	void testGetDataInicio() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetDataInicio() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetDataFim() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetDataFim() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetCliente() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetCliente() {
+		fail("Not yet implemented");
+	}
+
+}
