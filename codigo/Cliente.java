ad6d4ce8c84d504952d7d06e655210dd4602f075
coutinholaura <110981345+coutinholaura@users.noreply.github.com>
2023-09-05 12:51:21 -0300

    Update Cliente.java

--- codigo/Cliente.java
+++ codigo/Cliente.java
@@ -1,19 +1,24 @@
 package codigo;
 
 public class Cliente{
-    String nome = "";
-    String id = "";
-
-    public String getId() {
-        return id;
-    }
+	 private Long idCliente;
+	 private String nome;
+	 
     public String getNome() {
         return nome;
     }
-    public void setId(String id) {
-        this.id = id;
-    }
+   
     public void setNome(String nome) {
         this.nome = nome;
     }
+	public Long getIdCliente() {
+		return idCliente;
+	}
+	public void setIdCliente(Long idCliente) {
+		this.idCliente = idCliente;
+	}
+	 public static Cliente novoCliente (Long idCliente , String nome){
+	        Cliente cliente = new Cliente();
+	        return cliente;
+	    }
 }
